0ea69992b0ca43d9a23b6aa29838f22ee5fa259b
oCrazyoff <walyssonribeiro3@gmail.com>
2024-07-15 19:29:05 -0300

    fix erros de primeiro commit

--- js/menu.js
+++ js/menu.js
@@ -1,32 +1,15 @@
-<<<<<<< HEAD
 let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('menu-mobile')
 let overlay = document.getElementById('overlay-menu')
 
-btnMenu.addEventListener('click', ()=>{
+btnMenu.addEventListener('click', () => {
     menu.classList.add('abrir-menu')
 })
 
-menu.addEventListener('click', ()=>{
+menu.addEventListener('click', () => {
     menu.classList.remove('abrir-menu')
 })
 
-overlay.addEventListener('click', ()=>{
+overlay.addEventListener('click', () => {
     menu.classList.remove('abrir-menu')
-=======
-let btnMenu = document.getElementById('btn-menu')
-let menu = document.getElementById('menu-mobile')
-let overlay = document.getElementById('overlay-menu')
-
-btnMenu.addEventListener('click', ()=>{
-    menu.classList.add('abrir-menu')
-})
-
-menu.addEventListener('click', ()=>{
-    menu.classList.remove('abrir-menu')
-})
-
-overlay.addEventListener('click', ()=>{
-    menu.classList.remove('abrir-menu')
->>>>>>> 7bf2e4a6dafc0dffd783e7b37f4c034dda418afa
 })
--- js/roll.js
+++ js/roll.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //ROLAGEM PARA O PORTIFOLIO
 document.addEventListener('DOMContentLoaded', () => {
     const imgPorts = document.querySelectorAll('.img-port');
@@ -36,7 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     entry.target.classList.remove('active');
                 }
             });
-        }, { threshold: 0.8});
+        }, { threshold: 0.8 });
 
         dif.forEach(dif => {
             observer.observe(dif);
@@ -44,50 +43,3 @@ document.addEventListener('DOMContentLoaded', () => {
 
     }
 });
-=======
-//ROLAGEM PARA O PORTIFOLIO
-document.addEventListener('DOMContentLoaded', () => {
-    const imgPorts = document.querySelectorAll('.img-port');
-
-    if (window.innerWidth <= 1020) {
-
-        const observer = new IntersectionObserver((entries) => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    entry.target.classList.add('active');
-                } else {
-                    entry.target.classList.remove('active');
-                }
-            });
-        }, { threshold: 0.4 });
-
-        imgPorts.forEach(imgPort => {
-            observer.observe(imgPort);
-        });
-
-    }
-});
-
-//ROLAGEM PARA OS DIFERENCIAIS
-document.addEventListener('DOMContentLoaded', () => {
-    const dif = document.querySelectorAll('.diferenciais-box');
-
-    if (window.innerWidth <= 1020) {
-
-        const observer = new IntersectionObserver((entries) => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    entry.target.classList.add('active');
-                } else {
-                    entry.target.classList.remove('active');
-                }
-            });
-        }, { threshold: 0.8});
-
-        dif.forEach(dif => {
-            observer.observe(dif);
-        });
-
-    }
-});
->>>>>>> 7bf2e4a6dafc0dffd783e7b37f4c034dda418afa
